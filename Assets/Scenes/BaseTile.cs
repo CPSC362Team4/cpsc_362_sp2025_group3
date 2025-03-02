3b4b5d58ec246970c7ba9f157b183875899c2b1d
RoseB <mahsa.f@csu.fullerton.edu>
2025-03-01 22:19:22 -0800

    Update BaseTile.cs

--- Assets/Scenes/BaseTile.cs
+++ Assets/Scenes/BaseTile.cs
@@ -1,16 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+// abstract base class for all tiles
+public abstract class BaseTile : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum TileColor { Red, Blue, Green, Yellow, None }  // here I can define tile color
+    public TileColor color;  // this Stores tile color
+
+    protected List<GameObject> piecesOnTile = new List<GameObject>();  // stores pieces on this tile
+
+    // I will usde this method to add a piece to the tile
+    public void AddPiece(GameObject piece)
     {
-        
+        piecesOnTile.Add(piece);
     }
 
-    // Update is called once per frame
-    void Update()
+    // this is a method to remove a piece from the tile
+    public void RemovePiece(GameObject piece)
     {
-        
+        piecesOnTile.Remove(piece);
     }
+
+    // this is a method to get all pieces currently on the tile
+    public List<GameObject> GetPieces()
+    {
+        return piecesOnTile;
+    }
+
+    // Abstract method for tile effects (to be implemented in derived tile types)
+    public abstract void ApplyEffect();
 }
